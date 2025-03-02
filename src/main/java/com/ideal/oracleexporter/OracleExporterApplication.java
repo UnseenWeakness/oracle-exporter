be77ac5150064a2d91838cffbeb13569ad2e0716
dusk-x <en2213a@gmail.com>
2025-03-02 15:09:27 +0800

    add

---
+++ src/main/java/com/ideal/oracleexporter/OracleExporterApplication.java
@@ -0,0 +1,13 @@
+package com.ideal.oracleexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleExporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ideal/oracleexporter/OracleExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideal.oracleexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleExporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
